945bf1d668765528178aebe1d8c5c57468c54555
adiu <adiu_1024@163.com>
2021-03-22 22:58:29 +0800

    Preview and upload are supported

--- README.md
+++ README.md
@@ -9,37 +9,62 @@
   yarn add m-uploader-cos
   ```
 
-* ES6 import mode
+#### Use examples
+* Proxy interception
   ```JS
   import COSUpload from 'm-uploader-cos'
+  const handler = {
+    construct(target, args) {
+      const { headers = {}, ...config } = args[0]
+      config.headers = Object.assign(headers, {
+        'Authorization': localStorage.getItem('AUTH-TOKEN')
+      })
+      return new target(config)
+    }
+  }
+  const ProxyCOSUpload = new Proxy(COSUpload, handler)
+  export default ProxyCOSUpload
   ```
 
-#### Use examples
-* Basic usage
+* Upload files directly
   ```JS
   const COSUploader = new COSUpload({
     pickId: 'selector',  // id selector
-    authName: 'TOKEN',  // Token name of local storage
     tmpKeyUrl: '/api/upload/tmpKey'  // Get cos related configuration items through server interface
   })
   COSUploader.$on('loading', value => {
     console.log('COSUploading', value)
   })
-  COSUploader.$on('handleFileChange', filesInfo => {
+  COSUploader.$on('getUploadFileInfo', filesInfo => {
     console.log('filesInfo', filesInfo)
   })
   ```
   Note: through handlefilechange to get the file information for other operations, such as MD5 encryption
 
-* Get upload progress
+* Preview local files before uploading
+  ```JS
+  const COSUploader = new COSUpload({
+    pickId: 'selector',  // id selector
+    isPreview: true,
+    tmpKeyUrl: '/api/upload/tmpKey'  // Get cos related configuration items through server interface
+  })
+  COSUploader.$on('getPreviewFileInfo', filesInfo => {
+    console.log('previewFileInfo', filesInfo)
+  })
+  COSUploader.$on('uploadFileInfo', filesInfo => {
+    console.log('uploadFileInfo', filesInfo)
+  })
+  COSUploader.handleUpload(uploadFiles)
+  ```
+
+* Get upload progress and rate
   ```JS
   const COSUploader = new COSUpload({
     pickId: 'selector',
-    authName: 'TOKEN',
     tmpKeyUrl: '/api/upload/tmpKey',
     getProgress({ percentage, speed }) {
       console.log(`Upload progress：${percentage}`)
+      console.log(`Upload rate：${speed}`)
     }
   })
   ```
-  
--- es/index.js
+++ es/index.js
@@ -4,56 +4,61 @@ import { v4 as uuidv4 } from 'uuid'
 class COSUpload {
   constructor(config) {
     config.pickId = config.pickId.startsWith('#') ? config.pickId : `#${config.pickId}`
-    this.events = {}
     this.$options = config
+    this.events = {}
     this.init()
   }
   init() {
     const el = document.querySelector(this.$options.pickId)
     el.addEventListener('change', this.handleChange.bind(this), false)
   }
-  handleChange(e) {
-    this.$options.loading = true
-    const fileList = [...e.target.files]
-    e.target.value = ''
-    fileList.length && this.handleUpload(fileList)
+  handleChange() {
+    const { isPreview = false } = this.$options
+    const fileList = [...event.target.files]
+    event.target.value = ''
+    if (isPreview) {
+      this.$emit('getPreviewFileInfo', fileList)
+    } else {
+      this.handleUpload(fileList)
+    }
   }
   async handleUpload(fileList) {
     this.$emit('loading', true)
-    const { tmpKeyUrl, authName = 'TOKEN', getProgress = null } = this.$options
+    const { tmpKeyUrl, headers, SliceSize = 1024 * 1024, getProgress = null } = this.$options
     const {
-      bucket, region, requestAddress, dir, startTime, expiredTime, credentials
+      bucket, region, requestAddress, dir, credentials
     } = await fetch(tmpKeyUrl, {
-      headers: {
-        'Authorization': localStorage.getItem(authName)
+      mode: 'cors',
+      credentials: 'same-origin',
+      headers,
+    }).then(response => {
+      const { ok, status, statusText } = response
+      if (!ok) {
+        return Promise.reject({ status, statusText })
+      } else {
+        return response.json()
       }
-    }).then(res => res.json())
+    })
     const filesInfo = []
     const { tmpSecretId, tmpSecretKey, sessionToken } = credentials
     const files = fileList.map(file => {
-      const fileExtension = file.name.substring(file.name.lastIndexOf('.') + 1)
-      const newName = uuidv4().replace(/-/g, '') + '.' + fileExtension
-      const key = dir + '/' + newName
+      const uuid = uuidv4().replace(/-/g, '')
       filesInfo.push({
-        newname: newName,
-        fileExtension: fileExtension,
-        url: `${requestAddress}/${dir}/${newName}`,
-        file
+        url: `${requestAddress}/${dir}/${uuid}`,
+        file,
       })
-      return { Bucket: bucket, Region: region, Key: key, Body: file }
+      return { Bucket: bucket, Region: region, Key: `${dir}/${uuid}`, Body: file }
     })
-    this.$emit('handleFileChange', filesInfo)
+    this.$emit('getUploadFileInfo', filesInfo)
     const cos = new COS({
       SecretId: tmpSecretId,
       SecretKey: tmpSecretKey,
       XCosSecurityToken: sessionToken,
-      StartTime: startTime,
-      ExpiredTime: expiredTime,
       UploadCheckContentMd5: true
     })
     cos.uploadFiles({
       files,
-      SliceSize: 1024 * 1024,
+      SliceSize,
       StorageClass: 'STANDARD',
       onProgress: data => {
         const percentage = parseInt(data.percent * 100)
@@ -62,7 +67,9 @@ class COSUpload {
           getProgress({ percentage, speed })
         }
         if (percentage == 100) {
+          this.fileList = null
           this.$emit('loading', false)
+          this.$off()
         }
       }
     })
@@ -74,8 +81,8 @@ class COSUpload {
     const cbs = this.events[key]
     cbs && cbs.forEach(cb => cb.call(this, ...args))
   }
-  $off(key) {
-    delete this.events[key]
+  $off() {
+    this.events = null
   }
 }
 
